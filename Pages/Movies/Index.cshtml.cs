d67bc3a3925dc35ff68e55694f8265f78b66c9b2
jonty <501211312@qq.com>
2020-03-06 22:12:48 +0800

    条件查询

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -21,9 +22,33 @@ namespace RazorPagesMovie
 
         public IList<Movie> Movie { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            IQueryable<string> genreQuery = from m in _context.Movie
+                orderby m.Genre
+                select m.Genre;
+           // Movie = await _context.Movie.ToListAsync();
+           var movies = from m in _context.Movie
+               select m;
+           if (!string .IsNullOrEmpty(SearchString))
+           {
+               movies = movies.Where(s => s.Title.Contains(SearchString));
+           }
+
+           if (!string.IsNullOrEmpty(MovieGenre))
+           {
+               movies = movies.Where(x => x.Genre == MovieGenre);
+           }
+           Genres = new SelectList(await  genreQuery.Distinct().ToListAsync());
+           Movie = await movies.ToListAsync();
         }
     }
 }
