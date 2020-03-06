69c989fbf6df0d32ec5d4d4a7acc1d0274e4df14
jonty <501211312@qq.com>
2020-03-06 21:32:57 +0800

    生成页面

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +12,15 @@ namespace RazorPagesMovie.Models
         public int ID { get; set; }
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(dataType:DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+
+        /// <summary>
+        /// column 正确映射数据库中的货币
+        /// </summary>
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
